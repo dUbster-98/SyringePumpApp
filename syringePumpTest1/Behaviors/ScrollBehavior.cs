f4ce9b1354e5957951f88def1ef6bf6e03462d83
shkim369 <tjdgus981109@naver.com>
2024-11-06 18:01:22 +0900

    add: design

---
+++ syringePumpTest1/Behaviors/ScrollBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.Xaml.Behaviors;
+
+namespace SyringePumpTest1.Behaviors
+{
+    public class ScrollBehavior : Behavior<TextBox>
+    {
+        private static void AssociatedObject_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textbox = sender as TextBox;
+            if (textbox?.Text == null)
+                return;
+
+            Action action = () =>
+            {
+                textbox.UpdateLayout();
+
+                if (textbox.Text != null)
+                {
+                    textbox.ScrollToEnd();
+                }
+            };
+
+            textbox.Dispatcher.BeginInvoke(action);
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.TextChanged += AssociatedObject_TextChanged;
+        }
+
+        protected override void OnDetaching() 
+        {
+            base.OnDetaching();
+            AssociatedObject.TextChanged -= AssociatedObject_TextChanged;
+        }
+    }
+}
--- syringePumpTest1/ViewModels/MainViewModel.cs
+++ syringePumpTest1/ViewModels/MainViewModel.cs
@@ -3,6 +3,7 @@ using CommunityToolkit.Mvvm.Input;
 using SyringePumpTest1.Service;
 using SyringePumpTest1.Views;
 using SyringePumpTest1.Models;
+using SyringePumpTest1.Behaviors;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,10 @@ using System.Windows.Input;
 using SyringePumpTest1.Services;
 using CommunityToolkit.Mvvm.Messaging;
 using System.IO.Ports;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
+using System.Windows;
 
 namespace syringePumpTest1.ViewModels
 {
@@ -57,40 +62,82 @@ namespace syringePumpTest1.ViewModels
             set => SetProperty(ref _movePara, value);
         }
 
+        private int _downPara;
+        public int DownPara
+        {
+            get => _downPara;
+            set => SetProperty(ref _downPara, value);
+        }
+
+        private int _upPara;
+        public int UpPara
+        {
+            get => _upPara;
+            set => SetProperty(ref _upPara, value);
+        }
+
+        private int _lowerMargin;
+        public int LowerMargin
+        {
+            get => _lowerMargin;
+            set => SetProperty(ref _lowerMargin, value);
+        }
+
+        private int _upperMargin;
+        public int UpperMargin
+        {
+            get => _upperMargin;
+            set => SetProperty(ref _upperMargin, value);
+        }
+
+        private int _speedPara;
+        public int SpeedPara
+        {
+            get => _speedPara;
+            set => SetProperty(ref _speedPara, value);
+        }
+
+        private bool _isConnected;
+        public bool IsConnected
+        {
+            get => _isConnected;
+            set => SetProperty(ref _isConnected, value);
+        }
+
         private ICommand _openWindowCommand;
         public ICommand OpenWindowCommand
         {
-            get
-            {
-                return _openWindowCommand ?? (_openWindowCommand = new RelayCommand(OpenWindow));
-            }
+            get => _openWindowCommand ?? (_openWindowCommand = new RelayCommand(OpenWindow));
         }
 
         private ICommand _reconnectCommand;
         public ICommand ReconnectCommand
         {
-            get
-            {
-                return _reconnectCommand ?? (_reconnectCommand = new RelayCommand(SerialReconnect));
-            }
+            get => _reconnectCommand ?? (_reconnectCommand = new RelayCommand(SerialReconnect));
         }
 
         private ICommand _keyDownCommand;
         public ICommand KeyDownCommand
         {
-            get
-            {
-                return _keyDownCommand ?? (_keyDownCommand = new RelayCommand(EnterKeyDown));
-            }
+            get => _keyDownCommand ?? (_keyDownCommand = new RelayCommand(EnterKeyDown));
         }
 
         private ICommand _moveCommand;
         public ICommand MoveCommand
         {
-            get
-            {
-                return _moveCommand ?? (_moveCommand = new RelayCommand(PumpMove));
-            }
+            get => _moveCommand ?? (_moveCommand = new AsyncRelayCommand<object>(PumpMove));            
+        }
+
+        private ICommand _windowLoadedCommand;
+        public ICommand WindowLoadedCommand
+        {
+            get => _windowLoadedCommand ?? (_windowLoadedCommand = new RelayCommand(Window_Loaded));
+        }
+
+        private ICommand _setSpeedCommand;
+        public ICommand SetSpeedCommand
+        {
+            get => _setSpeedCommand ?? (_setSpeedCommand = new AsyncRelayCommand(SetSpeed));
         }
 
         private void OpenWindow()
@@ -123,12 +170,58 @@ namespace syringePumpTest1.ViewModels
             }
         }
         
-        private void PumpMove()
+        private async Task PumpMove(object dir)
         {
             try
             {
-                _serialService.SendData(_serialService.PumpSerial, $"/1A{MovePara.ToString()}R");
-                Task.Run(() => SerialReadAsync());
+                int para = 0;
+                switch (dir)
+                {
+                    case "A":
+                        para = MovePara;
+                        break;
+                    case "P":
+                        para = DownPara;
+                        break;
+                    case "D":
+                        para = UpPara;
+                        break;
+
+                    default:
+                        throw new Exception("Wrong direction");
+
+                }
+                _serialService.SendData(_serialService.PumpSerial, $"/1{dir}{para}R");
+                TextBoxAddText($">>>/1{dir}{para}R");
+                await SerialReadAsync();
+
+                await ParsingPosition();
+
+            }
+            catch (Exception ex)
+            {
+                TextBoxAddText(ex.Message);
+            }
+        }
+
+        private async Task ParsingPosition()
+        {
+            _serialService.SendData(_serialService.PumpSerial, $"/1?");
+            string res = await SerialReadAsync();
+            string resp = Regex.Replace(res, @"\p{C}+", "").Trim();
+            MovePara = int.Parse(resp.Substring(4));
+
+            UpperMargin = MovePara;
+            LowerMargin = 6000 - MovePara;
+        }
+
+        private async Task SetSpeed()
+        {
+            try
+            {
+                _serialService.SendData(_serialService.PumpSerial, $"/1S{SpeedPara}R");
+                TextBoxAddText($">>>/1S{SpeedPara}R");
+                await SerialReadAsync();
             }
             catch (Exception ex)
             {
@@ -140,13 +233,50 @@ namespace syringePumpTest1.ViewModels
         {
             _iniSetService = iniSetService;
             _serialService = serialService;
-            _textBoxService = textBoxService;
-                       
+            _textBoxService = textBoxService;            
+
             InputString = "";
             SerialLog = "";
 
             InitializeSerialPort();
 
+            Task.Run(()=> ParsingPosition());
+        }
+
+        private void Window_Loaded()
+        {
+            if (Application.Current.MainWindow is Window wnd)
+            {
+                WindowInteropHelper helper = new(wnd);
+                HwndSource source = HwndSource.FromHwnd(helper.Handle);
+                source.AddHook(new HwndSourceHook(WndProc));
+            }
+        }
+
+        private IntPtr WndProc(IntPtr hWnd, int nMsg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            uint WM_DEVICECHANGE = 0x0219;
+            uint DBT_DEVICEARRIVAL = 0x8000;
+            uint DBT_DEVICEREMOVECOMPLETE = 0x8004;
+
+            if (nMsg == WM_DEVICECHANGE)
+            {
+                //장치 변동
+                int wparam = wParam.ToInt32();
+                if (wparam == DBT_DEVICEARRIVAL || wparam == DBT_DEVICEREMOVECOMPLETE)
+                {
+                    if (wParam.ToInt32() == DBT_DEVICEARRIVAL)
+                    {
+                        TextBoxAddText("New Device Detected");
+                    }
+                    else if (wParam.ToInt32() == DBT_DEVICEREMOVECOMPLETE)
+                    {
+                        TextBoxAddText("Device Unpluged");
+                        IsConnected = false;
+                    }
+                }
+            }
+            return IntPtr.Zero;
         }
 
         private void InitializeSerialPort() 
@@ -161,6 +291,7 @@ namespace syringePumpTest1.ViewModels
             {
                 _serialService.OpenConnection(_serialService.PumpSerial, _serialService.serialSet.PumpSerialPort, _serialService.serialSet.PumpBoudRate);
                 TextBoxAddText("Pump Ready");
+                IsConnected = true;
             }
             catch (Exception ex) 
             {
@@ -170,7 +301,7 @@ namespace syringePumpTest1.ViewModels
 
         public void TextBoxAddText(string text)
         {
-            _textBoxService.AddText(text);
+            _textBoxService.AddText(text+"\r\n");
             OnPropertyChanged(nameof(TextBoxContext));
         }
         public void RemoveText()
@@ -180,7 +311,7 @@ namespace syringePumpTest1.ViewModels
         }
 
         static TaskCompletionSource<bool>? dataReceivedTaskCompletion;
-        public async Task<bool> SerialReadAsync()
+        public async Task<string> SerialReadAsync()
         {
             dataReceivedTaskCompletion = new TaskCompletionSource<bool>();
             _serialService.PumpSerial.DataReceived += DataReceivedHandler;
@@ -195,17 +326,16 @@ namespace syringePumpTest1.ViewModels
             if (dataReceivedTaskCompletion.Task.IsCompleted)
             {
                 TextBoxAddText("<<<" + data);
-                return true;
             }
             else
             {
                 TextBoxAddText("Data Read Fail");
-                return false;
             }
+            return data;
         }
         private static void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
         {
-            dataReceivedTaskCompletion.TrySetResult(true);  // 데이터 수신 작업을 완료함
+            dataReceivedTaskCompletion.TrySetResult(true);
         }
     }
 }
